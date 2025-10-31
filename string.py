f1a8c40d9185f7459ebe5c03113a677e86638171
Antu Mitra <130089045+mitra369@users.noreply.github.com>
2025-10-31 15:32:26 +0600

    Add files via upload

---
+++ string.py
@@ -0,0 +1,84 @@
+str1="Antu Mitra"
+str2="Nandinee Datta"
+
+'''1.Basic Input'''
+
+# str3='''Okay
+# i think you are fine
+# thank you'''
+
+# str4="""Definitely"""
+
+# print(str1)
+# print(str2)
+# print(str3)
+# print(str4)
+
+'''2.Repetation Operator'''
+# print(str1*5 )
+
+'''3.Concatation operator---Use coma when need space between strings and use + when there no space needed'''
+# print(str1,str2)
+# print(str1+str2)
+
+'''4.Comparing Operator'''
+# print(str1==str2)
+# print(str1>str2)
+# print(str1>=str2)
+# print(str1<str2)
+
+# str11="A"
+# str12="B"
+# print(str11<str12)
+
+'''5.UPPER,LOWER,TITLE'''  
+# print(str1.upper())   
+# print(str1.lower())
+# print(str1.swapcase())
+# print(str1.title()) 
+
+'''6.isupper/islower/ititle'''
+# print(str1.isupper())
+# print(str1.islower())
+# print(str2.istitle())
+
+'''7.isdigit/isalpha/isalnum/isspace'''
+# str0="antu443"
+# str00="1234"
+# print(str0.isalpha())
+# print(str0.isalnum())
+# print(str00.isdigit())
+
+'''8.isspace--Only space no word'''
+# space=" "
+# print(str1.isspace())
+# print(space.isspace())
+
+'''9.lstrip--left side space remove
+rstrip--rightside space remove
+strip--Both side space remove'''
+
+# space="   Antu Mitra"
+# space1="Antu Mitra  "
+# space2="  Antu Mitra  "
+
+# print(space.lstrip())
+# print(space1.rstrip())
+# print(space2.strip())
+
+'''10.replace/split/join'''
+
+# str0="Antu Mitra Nandinee Datta"
+# str00=("Antu","Mitra")
+# print(str2.replace("Nandinee","Mitra"))
+# print(str0.split())
+# print(" ".join(str00))
+
+'''11.startswith/endswith'''
+print(str1.startswith("An"))
+print(str2.endswith("tta"))
+
+
+
+
+                                                                                
